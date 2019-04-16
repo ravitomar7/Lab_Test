a50c5585e94a108807a13812506e53c1cf4284a9
Saumya Batra <twinkle.batra1424@gmail.com>
2019-04-16 07:57:21 +0200

    Add files via upload
    Name: Saumya Batra
    Branch: CSE BAO B3
    SAPID: 500062294
    Roll no: R103217095
    QUES: 3 AND 4

---
+++ class2.java
@@ -0,0 +1,11 @@
+package ques2;
+
+public class class2 extends ques3
+	{
+		public static void main(String args[])
+				{
+	        		System.out.println(ques3.makeBricks(2,1,4));
+	        		System.out.println(ques3.makeBricks(3,4,5));
+				}
+	
+	}
